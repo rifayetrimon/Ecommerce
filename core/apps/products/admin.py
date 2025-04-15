308f5d18349ec8da67c2ce8d69923947f4807657
rifayet <rifayetrimon88@gmail.com>
2025-04-15 18:04:20 +0800

    add product admin listing

--- core/apps/products/admin.py
+++ core/apps/products/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from core.apps.products.models import Product, Category, ProductImage, ProductStock, ProductVariant
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    readonly_fields = ('sku', 'stock')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'base_price', 'status', 'category')
+    inlines = [ProductImageInline, ProductVariantInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'use_as_menu')
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    search_fields = ('sku', 'variant_name', 'product__name')
+
+
+@admin.register(ProductStock)
+class ProductStockAdmin(admin.ModelAdmin):
+    list_display = ('product_variant', 'quantity')
+    autocomplete_fields = ('product_variant',)
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        varient = obj.product_variant
+        varient.stock += obj.quantity
+        varient.save()
--- core/apps/products/models.py
+++ core/apps/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
+class Category(models.Model): # Category model
     name = models.CharField(max_length=255)
     use_as_menu = models.BooleanField(default=False)
 
@@ -14,7 +14,7 @@ class Category(models.Model):
         return self.products.all()
 
 
-class Product(models.Model):
+class Product(models.Model): # Product model
     STATUS_CHOICES = [
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -30,4 +30,38 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+
+class ProductImage(models.Model): # Product Image model
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+
+
+class ProductVariant(models.Model): # Product Variant model
+    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
+    variant_name = models.CharField(max_length=255)
+    attributes = models.JSONField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=50, blank=True)
+    size = models.CharField(max_length=50, blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    sku = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.variant_name}"
     
+    def save (self, *args, **kwargs):
+        if not self.sku:
+            self.sku = uuid.uuid4().hex[:10].upper()
+        super().save(*args, **kwargs)
+
+class ProductStock(models.Model): # Product Stock model
+    product_variant = models.ForeignKey(ProductVariant, related_name='stocks', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self, *args, **kwargs):
+        return f"{self.product_variant.product.sku}"
