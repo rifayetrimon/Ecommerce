8fffb8f0aa70b8b15764e83698976fce27b11f1d
rifayet <rifayetrimon88@gmail.com>
2025-04-18 11:15:36 +0800

    add category searilizer

--- core/apps/products/serializers.py
+++ core/apps/products/serializers.py
@@ -2,3 +2,9 @@ from rest_framework import serializers
 from core.apps.products.models import Product, Category, ProductImage, ProductVariant
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'use_as_menu']
+        read_only_fields = ['id']
+
